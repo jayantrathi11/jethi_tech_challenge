01a4d40344d63ff12fbda26ed273cd1fe73b69f5
jayantrathi <jayantrathi955@gmail.com>
2021-12-31 18:59:55 +0530

    Initial

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:jethi_tech/helper/buttons.dart';
-
+import ;
 class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Quick',
+              style: GoogleFonts.libreBaskerville(
+                color: Colors.black,
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'News',
+              style: GoogleFonts.libreBaskerville(
+                color: Colors.red,
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
       body: Buttons(
         color: Colors.lightBlueAccent,
         str: 'Love',
